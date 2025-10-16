0744519ad909ae8b030fa2f616ec002adfced079
Ricardo Souza <rr_souza@outlook.com>
2025-10-16 15:06:26 -0300

    Correção definitiva para impedir navegação por Tab

--- src/app/shared/components/properties/type-property/property-integer/property-integer.component.ts
+++ src/app/shared/components/properties/type-property/property-integer/property-integer.component.ts
@@ -1,6 +1,13 @@
 import { takeUntil } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
-import { Component, EventEmitter, Input, OnInit, Output, OnDestroy } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  OnDestroy,
+} from '@angular/core';
 import { ResponsiveService } from 'src/app/shared/services/responsive.service';
 import { Subject } from 'rxjs';
 import { PropertyTemplateModel } from 'src/app/shared/models/PropertyTemplateModel';
@@ -9,27 +16,35 @@ import { TypePropertyModelEnum } from 'src/app/shared/enums/TypePropertyModelEnu
 @Component({
   selector: 'app-property-integer',
   templateUrl: './property-integer.component.html',
-  styleUrls: ['./property-integer.component.scss']
+  styleUrls: ['./property-integer.component.scss'],
 })
-export class PropertyIntegerComponent implements OnInit {
-
+export class PropertyIntegerComponent implements OnInit, OnDestroy {
   @Input() property: PropertyTemplateModel;
+
   @Output() changed = new EventEmitter();
+
   type = TypePropertyModelEnum;
+
   responsive: boolean;
+
   $destroy = new Subject();
+
   language: string;
 
   constructor(
     private responsiveSrv: ResponsiveService,
     private translateSrv: TranslateService
   ) {
-    this.responsiveSrv.observable.pipe(takeUntil(this.$destroy)).subscribe( value => {
-      this.responsive = value;
-    });
-    this.translateSrv.onLangChange.pipe(takeUntil(this.$destroy)).subscribe(() => {
-      setTimeout(() => this.setLanguage(), 200);
-    });
+    this.responsiveSrv.observable
+      .pipe(takeUntil(this.$destroy))
+      .subscribe((value) => {
+        this.responsive = value;
+      });
+    this.translateSrv.onLangChange
+      .pipe(takeUntil(this.$destroy))
+      .subscribe(() => {
+        setTimeout(() => this.setLanguage(), 200);
+      });
   }
 
   ngOnInit(): void {
@@ -44,7 +59,4 @@ export class PropertyIntegerComponent implements OnInit {
   setLanguage() {
     this.language = this.translateSrv.currentLang;
   }
-
-  
 }
-
