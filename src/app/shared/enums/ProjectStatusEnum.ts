23a18e79e72386d8368a94d84ce38a9ca6ab0978
Ricardo Souza <rr_souza@outlook.com>
2025-12-16 18:43:05 -0300

    Iniciado criação dos status de Projetos e Entregas

---
+++ src/app/shared/enums/ProjectStatusEnum.ts
@@ -0,0 +1,9 @@
+export enum ProjectStatus {
+  ESTRUTURACAO = 'Estruturação',
+  EXECUCAO = 'Execução',
+  SUSPENSO = 'Suspenso',
+  CANCELADO = 'Cancelado',
+  CONCLUIDO = 'Concluído',
+  PARALISADO = 'Paralisado',
+  PLANEJAMENTO = 'Planejamento',
+}
