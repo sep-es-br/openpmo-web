3fe4dd796a84b5b0b3d645c83cd51c098c959293
Bruno Matos <brunomatosbarbosa@hotmail.com>
2026-02-10 16:03:15 -0300

    Ajustes no recebimento e envio de processos

--- src/app/shared/interfaces/IProcess.ts
+++ src/app/shared/interfaces/IProcess.ts
@@ -1,14 +1,24 @@
 export interface IProcess {
   id?: number;
   idWorkpack?: number;
+
   processNumber: string;
   name?: string;
   subject?: string;
+
   currentOrganization: string;
   lengthOfStayOn?: number;
+
+  lengthOfStayOnSector?: number;
+  actingDate?: string | Date;
+  actingOrganization?: string;
+  actingSector?: string;
+  lastDispatchDate?: string | Date;
+
   note?: string;
   priority?: boolean;
   status?: string;
+
   history?: IProcessHistoryItem[];
 }
 
